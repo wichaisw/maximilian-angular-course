c8c24fa764c5ec5d3c66ac17c3098a058ccbff40
wichaisw <wichai.sw19@gmail.com>
2020-10-25 17:23:31 +0700

    add data binding: string interpolation, property binding, event binding, two-way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,17 +11,33 @@ import { Component, OnInit } from '@angular/core';
   
 // selector by ID & pseudor i.e. hover ใช้ไม่ได้
 
-  // templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
+
   // ถ้าจะtemplate ตัดบรรทัดต้องใช้ backtick
-  template: `<h1>inline template</h1>
-  <app-server></app-server>`,
+  // template: `<h1>inline template</h1>
+  // <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = "No server was created";
+  serverName: string = "Untitled";
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }

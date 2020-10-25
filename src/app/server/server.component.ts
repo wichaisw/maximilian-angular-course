552c14c52c2057d3292c65f8a8f209931aa8bac9
wichaisw <wichai.sw19@gmail.com>
2020-10-25 14:54:51 +0700

    solved conflict

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-<<<<<<< HEAD
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
 
 }
-=======
-    selector: 'app-server',
-    templateUrl: './server.component.html',
-})
-export class ServerComponent {
-
-}  
->>>>>>> 228772333751826a88509c9e2687dc997cdb0821
